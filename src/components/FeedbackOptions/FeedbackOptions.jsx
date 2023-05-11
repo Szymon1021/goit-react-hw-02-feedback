b898c59a3110fa5e67f41193f178aa622dc52b73
Szymon1021 <105548800+Szymon1021@users.noreply.github.com>
2023-05-11 15:34:14 +0200

    style

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
+import styles from './Feedback.module.css';
 
 export const Feedback = ({ options, onLeaveFeedback }) => {
   const optionsFeedback = options.map(name => {
     return (
-      <button onClick={() => onLeaveFeedback(name)} key={nanoid()}>
-        {name}
-      </button>
+      <ul className={styles.buttonsList} key={nanoid()}>
+        <li key={nanoid()}>
+          <button onClick={() => onLeaveFeedback(name)} key={nanoid()}>
+            {name}
+          </button>
+        </li>
+      </ul>
     );
   });
 
